03dc608f44c0ff8e61fa169629af20a46c31f182
Dunna Avinash <56786344+Avinash905@users.noreply.github.com>
2023-03-18 23:54:02 +0530

    files added

---
+++ client/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <a
+          href="https://dunna-avinash.netlify.app/"
+          target="_blank"
+        >
+          <img
+            src={viteLogo}
+            className="logo"
+            alt="Vite logo"
+          />
+        </a>
+        <a
+          href="https://github.com/Avinash905"
+          target="_blank"
+        >
+          <img
+            src={reactLogo}
+            className="logo react"
+            alt="React logo"
+          />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Testing vite react because create-react-app is not recommended by
+        official react docs.
+      </p>
+    </div>
+  );
+}
+
+export default App;
